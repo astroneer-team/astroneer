39314f01a27f58da8d057a1ed9e61e9104428fd0
Lucas Larangeira <lukearch@proton.me>
2024-05-02 16:06:47 -0300

    Add default template files and configurations

---
+++ templates/default/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    // Adicione aqui as regras personalizadas que você deseja aplicar
+  },
+};
--- templates/default/src/server.ts
+++ templates/default/src/server.ts
@@ -1,3 +1,4 @@
+import { logRequest } from '@astroneer/common';
 import { Astroneer } from '@astroneer/core';
 import { createServer, Server } from 'http';
 import { parse } from 'url';
@@ -9,6 +10,8 @@ export default async function server(
 ): Promise<Server> {
   const app = new Astroneer();
   const server = createServer(async (req, res) => {
+    if (devmode) logRequest(req, res);
+
     try {
       const parsedUrl = parse(req.url || '', true);
       await app.handle(req, res, parsedUrl);
