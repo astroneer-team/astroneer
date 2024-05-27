9e4a655e9a3afcfd68c5b117200f0834d6540b94
Lucas Larangeira <lukearch@proton.me>
2024-05-26 22:53:09 -0300

    Delete src directory

--- src/middlewares/middleware.ts
+++
@@ -1,5 +0,0 @@
-import { HttpError, RouteMiddleware } from '@astroneer/core';
-
-export const middleware: RouteMiddleware = async () => {
-  throw new HttpError(401, 'Not Found');
-};
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { RouteMiddleware } from '@astroneer/core';
-
-export const middlewares: RouteMiddleware[] = [];
-
-export async function GET() {}
-
-export const POST = async () => {};
