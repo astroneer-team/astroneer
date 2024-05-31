debcfbd33fdfc6566128773eef881c55c12ff512
Lucas Larangeira <lukearch@proton.me>
2024-05-31 15:20:11 -0300

    chore: Remove printVersion function and update CLI version logging

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,12 +2,14 @@
 import { Command } from 'commander';
 import { readFileSync } from 'fs';
 import path from 'path';
+import picocolors from 'picocolors';
 import { buildCmd, devCmd, startCmd } from './commands';
 
 async function astroneerCLI() {
   const packageJsonPath = path.resolve(__dirname, '../package.json');
   const packageJson = readFileSync(packageJsonPath, 'utf-8');
   const { version, description, name } = JSON.parse(packageJson);
+  console.log(picocolors.blue(`>_  Astroneer.js CLI v${version}\n`));
   const program = new Command(name)
     .description(description)
     .version(version)
